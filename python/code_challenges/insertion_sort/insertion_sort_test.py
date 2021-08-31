2b4d4fc309c965b66cbddc22e2cb4c2ff385196d
Nadeemmazahreh <nadeemmazahreh97@hotmail.com>
2021-08-31 15:05:50 +0300

    attempt 1

--- python/code_challenges/insertion_sort/insertion_sort_test.py
+++
@@ -1,9 +0,0 @@
-from insertion_sort import insertionSort
-
-def test_insertionSort():
-    arr = [8,4,23,42,16,15]
-    expected = [4,8,15,16,23,42]
-    actual = insertionSort(arr)
-    assert actual == expected
-
-
